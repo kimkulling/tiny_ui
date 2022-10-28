0c4b69646d752293817acd5bf0033ae4e1489c15
Kim Kulling <kim.kulling@googlemail.com>
2022-10-28 21:18:26 +0200

    Add missing file

---
+++ src/tinyui.cpp
@@ -0,0 +1,14 @@
+#include "tinyui.h"
+#include "widgets.h"
+
+namespace tinyui {
+
+int tui_init(tui_context &ctx) {
+    return 0;
+}
+
+int tui_release(tui_context &ctx) {
+    return 0;
+}
+
+} // namespace tinyui
