7fdfb032f2f41cd2acc146294675d2a07c79a906
Kim Kulling <kim.kulling@draeger.com>
2023-07-14 14:47:31 +0200

    Separate responibilities a little bit more

--- src/sdl2_renderer.h
+++ src/sdl2_renderer.h
@@ -16,7 +16,7 @@ struct Renderer {
     static tui_ret_code releaseRenderer(tui_context &ctx);
     static tui_ret_code initScreen(tui_context &ctx, int32_t x, int32_t y, int32_t w, int32_t h);
     static tui_ret_code initScreen(tui_context &ctx, SDL_Window *mWindow, SDL_Renderer *mRenderer);
-    static tui_ret_code drawText(tui_context &ctx, const char *string, int32_t size, const tui_rect &r, const SDL_Color &fgC, const SDL_Color &bgC);
+    static tui_ret_code drawText(tui_context &ctx, const char *string, tui_font *font, const tui_rect &r, const SDL_Color &fgC, const SDL_Color &bgC);
     static tui_ret_code drawRect(tui_context &ctx, int32_t x, int32_t y, int32_t w, int32_t h, bool filled, tui_color4 fg);
     static tui_ret_code drawImage(tui_context &ctx, tui_image *image);
     static tui_ret_code beginRender(tui_context &ctx, tui_color4 bg);
